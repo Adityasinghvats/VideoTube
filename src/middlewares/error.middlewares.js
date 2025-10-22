a3c04e6f728ecad5e8df40e2090bf62c6c0338be
Adityasinghvats <131326798+Adityasinghvats@users.noreply.github.com>
2025-10-22 22:35:50 +0530

    feat: add missing dashboard controller for stats for the video

--- src/middlewares/error.middlewares.js
+++ src/middlewares/error.middlewares.js
@@ -1,24 +1,26 @@
 import mongoose from "mongoose";
 import { ApiError } from "../utils/ApiError.js";
+import { logger } from "../logger.js";
 
 const errorHandler = (err, req, res, next) => {
     let error = err
-    if(!(error instanceof ApiError)){
+    if (!(error instanceof ApiError)) {
         //manipulating error code and message from ApiError
         const statusCode = error.statusCode || error instanceof mongoose.Error ? 400 : 500;
 
-        const message = error.message ||"Something went wrong"
+        const message = error.message || "Something went wrong"
 
         error = new ApiError(statusCode, message, error?.errors || [], err.stack)
     }
+    logger.error(`Error occurred: ${error.message}`, { stack: error.stack, errors: error.errors });
 
     const response = {
         ...error,
         message: error.message,
-        ...(process.env.NODE_ENV === "development" ? {stack: error.stack} : {})
+        ...(process.env.NODE_ENV === "development" ? { stack: error.stack } : {})
     }
 
     return res.status(error.statusCode).json(response)
 }
 
-export {errorHandler}
+export { errorHandler }
